fa79bf7ae62407b3081f1dfd99e25664e2e53fc1
Aman Sharma <aman.sharma0091@gmail.com>
2015-12-20 09:03:05 +0530

    Add bootstrap4, jquery, timeEntry modules

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,13 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import {Component}         from 'angular2/core';
-import {TaskComponent} from './task.component';
-
+import {TaskComponent} from './task.component'
+// import {HeroFormComponent} from './hero-form.component'
+// @Component({
+//   selector: 'my-app',
+//   template: '<hero-form></hero-form>',
+//   directives: [HeroFormComponent]
+// })
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
